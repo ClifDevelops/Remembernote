d2feae3bcc329cbdcea952c5f6905bb7ae09cad8
Robert Burroughs <clifdevelops@gmail.com>
2021-04-03 15:27:55 -0600

    got the demo user to work

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Redirect } from "react-router-dom";
 import { setMemories } from "../../store/memories";
 import Sidebar from "../Sidebar"
 import MemoryList from "../MemoryList"
@@ -8,8 +8,8 @@ import MemoryForm from "../MemoryForm"
 import "./HomePage.css";
 
 const Homepage = ({isLoaded}) => {
-
-  
+const sessionUser = useSelector((state) => state.session.user);
+if (!sessionUser) return <Redirect to="/" />; 
 
     return (
       <div className="homepage-container">
--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,7 +1,23 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import DemoLogin from "../DemoLogin";
 import "./Splash.css"
 
 function Splash() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("demo@user.io");
+  const [password, setPassword] = useState("password");
+
+  if (sessionUser) return <Redirect to="/homepage" />;
+
+  const loginDemoUser = () => {
+    return dispatch(sessionActions.login({ credential, password }));
+  }
+
     return (
       <div className="splash-container">
         <div className="splash-banner">REMEMBERNOTE</div>
@@ -12,6 +28,8 @@ function Splash() {
         <NavLink to="/signup" className="splash-link">
           Sign Up
         </NavLink>
+        <button onClick={loginDemoUser}>Try out as a demo user!</button>
+        {/* <DemoLogin /> */}
       </div>
     );
 }

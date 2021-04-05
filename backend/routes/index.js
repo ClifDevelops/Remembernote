b46bfa2dc6946cdd237aec1c36ad5191c3265ba0
Robert Burroughs <clifdevelops@gmail.com>
2021-04-05 09:25:33 -0600

    heroku bugfix

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const apiRouter = require("./api");
 
-router.use("/api", apiRouter);
+
 
 // Static routes
 // Serve React build files in production
@@ -35,5 +35,5 @@ if (process.env.NODE_ENV !== "production") {
     res.status(201).json({});
   });
 }
-
+router.use("/api", apiRouter);
 module.exports = router;
